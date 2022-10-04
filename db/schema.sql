0f2cab5131b3e8919d0bab68663a528bc1cd36ab
chaz Graham <chazhg80.cg@gmail.com>
2022-10-04 17:32:40 -0500

    added job_title table and added values

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,13 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
-
+CREATE TABLE job_title (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    department_id INT NOT NULL,
+    salary DECIMAL NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
