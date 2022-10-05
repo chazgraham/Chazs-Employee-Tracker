022d121b0f5b17821f440a7df40ce3980d10a92e
chaz Graham <chazhg80.cg@gmail.com>
2022-10-04 19:07:08 -0500

    added tracking to user slected option

--- server.js
+++ server.js
@@ -20,8 +20,27 @@ const promptOptions = () => {
         {
             name: 'options',
             type: 'list',
-            message: 'What would you like to do? (use arrow keys)',
-            choices: ['View All Employees', 'Add Employee', 'Update Employee', 'View All Job Titles', 'Add Job Title', 'View All Departments', 'Add Department']
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Quit']
         }
     ])
+    .then(choice => {
+        if(choice.options === 'View All Departments') {
+            console.log('View All Departments chosen');
+        }else if(choice.options === 'View All Roles') {
+            console.log('View All Roles chosen');
+        }else if(choice.options === 'View All Employees') {
+            console.log('View All Employees chosen');
+        }else if(choice.options === 'Add a Department') {
+            console.log('Add a Department chosen');
+        }else if(choice.options === 'Add a Role') {
+            console.log('Add a Role chosen');
+        }else if(choice.options === 'Add an Employee') {
+            console.log('Add an Employee chosen');
+        }else if(choice.options === 'Update an Employee Role') {
+            console.log('Update an Employee Role chosen');
+        }else if(choice.options === 'Quit') {
+            connection.end();
+        }
+    });
 } 
